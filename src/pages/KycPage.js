4db8b544c3d832da5b560cdecad07a715700fa24
Savan-Savani <savsnsavani111@gmail.com>
2022-10-07 16:11:32 -0400

    frontend complete

---
+++ src/pages/KycPage.js
@@ -0,0 +1,11 @@
+import KycHomeComponent from "../components/KycComponent/KycHomeComponent";
+
+const KycPage = () => {
+  return (
+    <div >
+      <KycHomeComponent />
+    </div>
+  );
+}
+
+export default KycPage;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,17 @@
+import Footer from '../components/LandingPageComponents/Footer';
+import Header from '../components/LandingPageComponents/Header';
+import HomePage from '../components/LandingPageComponents/Home';
+
+function LandingPage() {
+  return (
+    <div className="is-preload landing">
+      <div id="page-wrapper">
+        <Header />
+        <HomePage />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
